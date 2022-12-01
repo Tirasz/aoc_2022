33ac7bf7a2fa61c0b9e69f53b39f993dca372974
Tirasz <zacsi143@gmail.com>
2022-12-01 13:05:22 +0100

    day 1

--- 1/solution.py
+++ 1/solution.py
@@ -1,1 +1,19 @@
 # https://adventofcode.com/2022/day/1
+
+CALORIES = {}
+ELVES = 0
+CALORIES[0] = 0
+
+with open('input.txt') as f:
+    for line in f:
+        if not line.strip():
+            ELVES += 1
+            CALORIES[ELVES] = 0
+            continue
+        CALORIES[ELVES] += int(line)
+
+
+sorted = list(CALORIES.values())
+sorted.sort()
+print(sorted[-1])
+print(sorted[-1] + sorted[-2] + sorted[-3])
